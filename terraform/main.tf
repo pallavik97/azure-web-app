c0c4f14a0be7aebbac8b6c906eedf234c31f2fda
pallavik97 <127023899+pallavik97@users.noreply.github.com>
2024-02-08 13:25:48 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-cluster"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "example"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
